229566f6fa32715745dd25fe6e9d09f4a20cad3d
Adrian <45426088+adryCrespo@users.noreply.github.com>
2024-12-12 13:17:27 +0100

    uami11 - 2

--- database/pruebaz.py
+++ database/pruebaz.py
@@ -9,13 +9,13 @@ uami2_clientID="8718f41f-38f7-4fb9-ba85-aabe535ec5f1"
 # connection_string = f"Driver={ODBC Driver 18 for SQL Server};Server=tcp:server201124.database.windows.net,1433;Database=db201124;Uid={uami2_clientID};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryIntegrated"
 # connection_string = 'Server=tcp:server201124.database.windows.net,1433;Initial Catalog=db201124;Persist Security Info=False;User ID=8718f41f-38f7-4fb9-ba85-aabe535ec5f1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication="Active Directory Managed Identity";'
 
-server = "tcp:server201124.database.windows.net"
+server = "tcp:tiempo-server.database.windows.net"
 port = 1433
-database = "db201124"
+database = "tiempoDB"
 client_id = uami2_clientID
 authentication = 'ActiveDirectoryIntegrated'
 # authentication = 'ActiveDirectoryMsi'
-connString = os.environ["AZURE_SQL_CONNECTIONSTRING"]
+# connString = os.environ["AZURE_SQL_CONNECTIONSTRING"]
 connString = f'Driver={{ODBC Driver 18 for SQL Server}};Server={server},{port};Database={database};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
 
@@ -33,15 +33,31 @@ def get_conn():
 
 # engine = create_engine(conn, echo=True)    
 # sql = text(f"select * from dbo.ciudades where ciudad=madrid")
+
+import pandas as pd
+df = pd.read_csv("database/datos_nuevos.csv")
+data = df.apply(tuple,axis=1).tolist()
+
+sql = "INSERT INTO dbo.ciudades (fecha, ciudad, descripcion, t_min, t_max) VALUES (?, ?, ?,?,?)"
+params = [tuple(row) for row in data]
 with get_conn() as connection:
     cursor = connection.cursor()
-    cursor.execute("select * from dbo.ciudades where nombre='madrid' ")
-    rows = cursor.fetchall()
+    cursor.executemany(sql, params)
+    connection.commit()
 
-    # cursor = conn.cursor()
-    # result = cursor.execute("select * from dbo.ciudades where ciudad='madrid' ").fetchall()
 
-print(rows)
+
+
+# with get_conn() as connection:
+#     cursor = connection.cursor()
+#     # cursor.execute("select * from dbo.ciudades where ciudad='madrid' ")
+#     cursor.execute("INSERT INTO dbo.ciudades")
+#     rows = cursor.fetchall()
+
+#     # cursor = conn.cursor()
+#     # result = cursor.execute("select * from dbo.ciudades where ciudad='madrid' ").fetchall()
+
+# print(rows)
 # llamada simple
 
 # credential with uami2_clientID
