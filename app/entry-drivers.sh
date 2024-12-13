6de6652279b7af697882fb0b0a0dbd1c35c7015a
Adrian <45426088+adryCrespo@users.noreply.github.com>
2024-12-13 16:14:37 +0100

    query data 8, drivers

--- app/entry-drivers.sh
+++ app/entry-drivers.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Debian 9-11
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+
 #Debian 11
 curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
