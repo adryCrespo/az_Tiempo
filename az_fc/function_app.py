830a2b7b53c394b0c4187f826b8f07df4d7d7fab
Adrian <45426088+adryCrespo@users.noreply.github.com>
2024-12-17 10:06:31 +0100

    azfc 20

--- az_fc/function_app.py
+++ az_fc/function_app.py
@@ -8,7 +8,7 @@ CIUDADES = ["madrid","alcala_heranes","getafe","collado_villalba","navalcarnero"
 
 app = func.FunctionApp()
 @app.function_name(name="dbTimer")
-@app.timer_trigger(schedule="0 10,20 20 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+@app.timer_trigger(schedule="0 10,20 9 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
 @app.generic_output_binding(arg_name="itemCiudad", type="sql", CommandText="dbo.ciudades", ConnectionStringSetting="SqlConnectionString")
 def timer_trigger(myTimer: func.TimerRequest,itemCiudad: func.Out[func.SqlRow]) -> None:
     if myTimer.past_due:
@@ -25,6 +25,8 @@ def timer_trigger(myTimer: func.TimerRequest,itemCiudad: func.Out[func.SqlRow]) 
     sql_rows = []
     for ciudad in resumen.ciudades:
         ciudad_dict = resumen.ciudades[ciudad].toSQL()
+        temp_fecha = ciudad_dict["fecha"]
+        ciudad_dict["fecha"] = cambio_fecha(temp_fecha)
         logging.info(ciudad_dict)
         sql_rows.append(func.SqlRow(ciudad_dict) )
         logging.info(f'{ciudad} preparada en db')
@@ -35,3 +37,7 @@ def timer_trigger(myTimer: func.TimerRequest,itemCiudad: func.Out[func.SqlRow]) 
 
 
     logging.info('Python timer trigger function executed.')
+
+def cambio_fecha(fecha:str):
+    dia, mes, year = fecha.split("-")
+    return f"{year}-{mes}-{dia}"
